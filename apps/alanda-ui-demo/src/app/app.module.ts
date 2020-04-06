8ef360b43f6796ff9fe011f1734db21efb435b1e
ykoitzsch <yannick.koitzsch@bpmasters.at>
2020-04-06 10:19:33 +0200

    generate test component

--- apps/alanda-ui-demo/src/app/app.module.ts
+++ apps/alanda-ui-demo/src/app/app.module.ts
@@ -1,11 +1,14 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { AlandaUiModule } from '@alanda-io/alanda-ui';
 import { AppComponent } from './app.component';
 
 @NgModule({
   declarations: [AppComponent],
-  imports: [BrowserModule],
+  imports: [
+    BrowserModule,
+    AlandaUiModule
+  ],
   providers: [],
   bootstrap: [AppComponent]
 })
--- libs/alanda-ui/src/lib/alanda-ui.module.ts
+++ libs/alanda-ui/src/lib/alanda-ui.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { TestComponent } from './test/test.component';
 
 @NgModule({
-  imports: [CommonModule]
+  imports: [CommonModule],
+  declarations: [TestComponent],
+  exports: [TestComponent]
 })
 export class AlandaUiModule {}
